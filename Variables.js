fb2945f30ed70c0df313fa6acf925efa9b023c81
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-08-22 20:35:24 +0530

    Created variables.js and added basics

---
+++ Variables.js
@@ -0,0 +1,28 @@
+console.log(5 + 4);
+console.log(5 + 4 + 5);
+
+let num = 2 + 3;
+console.log(num);
+console.log(num + 10);
+
+var userName = "JD";
+console.log(userName);
+console.log(userName + " is a great programmer");
+
+// ctrl + / = to comment out a line of code
+
+var num1 = 5;
+console.log(num1);
+num1 = 10;
+console.log(num1);
+
+let userName1 = "Seeker";
+console.log(userName1);
+userName1 = "Neon";
+console.log(userName1);
+
+var $test = 5;
+var test$ = 5;
+
+let _test = 10;
+let test_ = 10;
