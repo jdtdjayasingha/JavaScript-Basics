2a7679f98571e1835d58001656b8af382ca1c673
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-08-24 14:51:55 +0530

    Explained Primitive data type

---
+++ 04.Datatypes.js
@@ -0,0 +1,30 @@
+let num = 100; //Number
+let str = "Hello"; //String
+
+console.log(num + 100);
+console.log(num - 500);
+console.log(num * 2);
+console.log(num / 9);
+
+let float = 5.144; //Float
+
+console.log(float + 100);
+console.log(float - 500);
+console.log(float * 2);
+console.log(float / 9);
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof float);
+
+let hex = 0x123; //Hexadecimal
+console.log(hex);
+
+let inf = 50 / 0;
+console.log(inf);
+
+let inf2 = -50 / 0;
+console.log(inf2);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_VALUE * 2);
