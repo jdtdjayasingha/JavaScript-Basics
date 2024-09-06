ad468526e7281cc7a5fb5aa960e11433bcb9aa3c
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-09-06 14:45:24 +0530

    Created Functions.js

---
+++ 05.Functions.js
@@ -0,0 +1,42 @@
+// Define a function
+function hello() {
+    console.log("Hello");
+}
+
+// Define a function with parameters
+function helloForStudent(name) {
+    console.log("Hello " + name);
+    console.log(`Hello ${name}`);
+}
+
+// Create a function that returns the sum of 2 numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Create a function that calculates the area of a circle
+function areaOfCircle(Pi, radius) {
+    area = Pi * radius * radius;
+    return area;
+}
+
+
+
+
+
+
+
+
+
+
+
+// Call the function
+hello();
+helloForStudent("Tharaka");
+
+let sum = add(5, 10);
+console.log(sum);
+console.log(add(5, 10));
+
+console.log(areaOfCircle(3.14, 10));
+
